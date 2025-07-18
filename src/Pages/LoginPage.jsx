da3473d02913bd3389b1d43c90e5d564e7e576bd
hasnain-a7 <husnainahmed519@gmail.com>
2025-07-18 23:04:42 +0500

    add mini header

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -2,7 +2,13 @@ import Login from "../Components/OtherComponents/Login";
 
 const LoginPage = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center h-screen transition-all w-full ">
+    <div className="relative flex flex-col items-center justify-center h-screen transition-all w-full">
+      {/* Mini bar at top */}
+      <div className="absolute top-0 w-full bg-gray-200 text-center text-sm text-gray-700 py-2 shadow-sm">
+        Roll No: <span className="font-semibold">E29</span> &nbsp; | &nbsp;
+        Password: <span className="font-semibold">Hasnain</span>
+      </div>
+
       <Login />
     </div>
   );
